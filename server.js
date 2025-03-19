5d22e783b8db82c1bb8ff46d11214c9d6587a345
Samuel Camargo <samuelcamargo1@gmail.com>
2025-03-19 17:26:04 -0300

    preparando para vercel

---
+++ server.js
@@ -0,0 +1,5 @@
+// Este arquivo serve como um ponto de entrada simplificado para o Vercel
+// Ele apenas importa a aplicação compilada a partir do diretório dist
+
+const app = require('./dist/index.js');
+module.exports = app; 
